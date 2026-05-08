e4ec4e47c3fa53394aaeb8bcf923bc070686938c
Darryl York III <darrylyork3@gmail.com>
2026-05-08 16:50:13 -0500

    Updated Protections

--- js/custom.js
+++ js/custom.js
@@ -71,16 +71,6 @@ if (typeof $.fn.owlCarousel === 'function' && $(".client_owl-carousel").length) 
     });
 }
 
-/** google_map js **/
-
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
-
 // Attempt at form-sheets submission
 // async function signup(){
 //   var form = document.getElementById("sheetdb-form");
